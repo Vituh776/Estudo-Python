a65354d967e5e501103f46f9e39e12aa29114770
Vituh776 <victorhenricke028@gmail.com>
2026-07-12 10:13:44 -0300

    Des 074,075

---
+++ pacote dowload/pythonProject/Desafios/Des076.py
@@ -0,0 +1,18 @@
+produtos = ('lapis', 1.75,
+            'Borracha', 2.00,
+            'Caderno', 15.90,
+            'Estojo', 25.00,
+            'Transferidor', 4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.50,
+            'canetinha', 22.40,
+            'Livro', 35.60)
+print('-' * 40)
+print(f'{"Tabela de Valores":^40}')
+print('-' * 40)
+for pos in range(0, len(produtos)):
+    if pos % 2 == 0:
+        print(f'{produtos[pos]:.<30}',end='')
+    else:
+        print(f'{produtos[pos]:>7.2f}')
+print('-' * 40)
